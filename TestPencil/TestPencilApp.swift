ef26f5815c06334acf4583889c0fc9b9039a727b
Longo97 <marco.longobardi997@gmail.com>
2025-09-17 11:47:30 +0200

    Initial Commit

---
+++ TestPencil/TestPencilApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPencilApp.swift
+//  TestPencil
+//
+//  Created by Marco Longobardi on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TestPencilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
